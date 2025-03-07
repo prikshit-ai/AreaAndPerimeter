cdbe6bce54ae801c246798218accf99269915b67
prikshitgaur772@gmail.com <prikshitgaur772@gmail.com>
2025-03-07 22:48:36 +0530

    Cylinder

---
+++ Cylinder.java
@@ -0,0 +1,17 @@
+public class Cylinder extends Shape implements Volume {
+    public Cylinder(double radius, double height) {
+        super(radius, height, 0, 0);
+    }
+
+    public double calculateArea() {
+        return 2 * Math.PI * dim_one * (dim_one + dim_two);
+    }
+
+    public double calculatePerimeter() {
+        return 2 * Math.PI * dim_one; // Circumference of base
+    }
+
+    public double calculateVolume() {
+        return Math.PI * dim_one * dim_one * dim_two;
+    }
+}
